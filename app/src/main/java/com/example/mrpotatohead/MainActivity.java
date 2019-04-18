1d203da1273ac722ca54f4efa9958b7fa2d00df3
wesselrijk <wesselrijk@gmail.com>
2019-04-18 19:45:37 +0200

    Final version

--- app/src/main/java/com/example/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/mrpotatohead/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.mrpotatohead;
-// hoi
+/*
+The main activity for this app takes place in this file. This is the only java file for this app,
+which means that the whole program takes place in this java file.
+The app is built fully in the MainActivity class, where firstly in the onCreate the layout is set
+and it is checked whether there is a savedInstance. Next the MainActivity will check if/when
+checkboxes are being checked and will change the visibility of drawables, creating the MrPotato
+character. At last the current instance is saved.
+*/
+
+// list of imports
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+
+// call the main activity
 public class MainActivity extends AppCompatActivity {
 
+    // instantiate the app with the onCreate, set the layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ImageViews of Mr Potato's parts
         ImageView image = findViewById(R.id.arms);
         ImageView image2 = findViewById(R.id.eyes);
         ImageView image3 = findViewById(R.id.eyebrows);
@@ -24,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView image9 = findViewById(R.id.nose);
         ImageView image10 = findViewById(R.id.shoes);
 
+        // check if there is a savedInstanceState, if this is the case, set visibility of ImageViews
+        // according to the saved visibility.
         if (savedInstanceState != null) {
             image.setVisibility(savedInstanceState.getInt("arms"));
             image2.setVisibility(savedInstanceState.getInt("eyes"));
@@ -38,55 +53,61 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // method specifying what happens when a checkbox is clicked
     public void checkClicked(View v) {
-        Log.d("halloooo", "clicked box");
+
+        // instantiate an id of the View that was clicked, also instantiate a new int variable,
+        // meant for the id of the ImageView corresponding to its checkbox, which is checked with a
+        // switch
         int id = v.getId();
-        int id_im;
+        int idImage;
         switch(id){
-            case R.id.eyes_cb:
-                id_im = R.id.eyes;
+            case R.id.eyesCb:
+                idImage = R.id.eyes;
                 break;
-            case R.id.arms_cb:
-                id_im = R.id.arms;
+            case R.id.armsCb:
+                idImage = R.id.arms;
                 break;
-            case R.id.ears_cb:
-                id_im = R.id.ears;
+            case R.id.earsCb:
+                idImage = R.id.ears;
                 break;
-            case R.id.eyebrows_cb:
-                id_im = R.id.eyebrows;
+            case R.id.eyebrowsCb:
+                idImage = R.id.eyebrows;
                 break;
-            case R.id.glasses_cb :
-                id_im = R.id.glasses;
+            case R.id.glassesCb :
+                idImage = R.id.glasses;
                 break;
-            case R.id.hat_cb:
-                id_im = R.id.hat;
+            case R.id.hatCb:
+                idImage = R.id.hat;
                 break;
-            case R.id.mustache_cb:
-                id_im = R.id.mustache;
+            case R.id.mustacheCb:
+                idImage = R.id.mustache;
                 break;
-            case R.id.mouth_cb:
-                id_im = R.id.mouth;
+            case R.id.mouthCb:
+                idImage = R.id.mouth;
                 break;
-            case R.id.nose_cb:
-                id_im = R.id.nose;
+            case R.id.noseCb:
+                idImage = R.id.nose;
                 break;
-            case R.id.shoes_cb:
-                id_im = R.id.shoes;
+            case R.id.shoesCb:
+                idImage = R.id.shoes;
                 break;
             default:
-                id_im = R.id.arms;
+                idImage = R.id.arms;
         }
-        ImageView image = findViewById(id_im);
+
+        // set visibility opposite of its current state for the ImageView corresponding to the View
+        // of the checkbox
+        ImageView image = findViewById(idImage);
         if (image.getVisibility() == View.INVISIBLE) {
-            Log.d("halloooo",  "invisible");
             image.setVisibility(View.VISIBLE);
         }
         else {
-            Log.d("halloooo",  "visible");
             image.setVisibility(View.INVISIBLE);
         }
     }
 
+    // save current settings in an onSaveInstanceState
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState); // always call super
 
